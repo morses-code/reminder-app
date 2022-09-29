5681075c30c5e732aef8a12b784d134654f1e960
morses-code <morsescott88@gmail.com>
2022-09-29 18:27:24 +0100

    added hello endpoint

--- api/server/server.go
+++ api/server/server.go
@@ -1,7 +1,12 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/morses-code/reminder-app/api"
+)
 
 func Server() {
+	http.HandleFunc("/hello", api.Hello)
 	http.ListenAndServe(":8080", nil)
 }
